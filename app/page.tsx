bfa9a13ce289549e85d5d131334e0ab0787a7eab
KhanRabiul <krabiulislam2@gmail.com>
2025-05-01 22:58:45 +0600

    basic stacture

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Moon } from 'lucide-react'
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { AvatarFallback } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
-<>
-<div>Home page</div>
-</>
+    <nav>
+      {/* Left */}
+      collapseButton
+      {/* Right */}
+      <div className="flex items-center gap-4">
+        <Link href='/dashboard'>Dashboard</Link>
+        <Moon />
+        <Avatar>
+          <AvatarImage
+            className="size-8"
+            src='https://img.freepik.com/free-vector/smiling-boy-yellow-hoodie_1308-175701.jpg?semt=ais_hybrid&w=740' />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
   );
 }
