2dbb0f38d92c48de22858d2bb7e30139d472fce0
KhanRabiul <krabiulislam2@gmail.com>
2025-05-02 22:09:12 +0600

    (Navbar.tsx) Tehming

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Moon } from 'lucide-react'
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
-import { AvatarFallback } from "@/components/ui/avatar";
+import Navbar from "@/components/Navbar";
+
 
 export default function Home() {
   return (
-    <nav>
-      {/* Left */}
-      collapseButton
-      {/* Right */}
-      <div className="flex items-center gap-4">
-        <Link href='/dashboard'>Dashboard</Link>
-        <Moon />
-        <Avatar>
-          <AvatarImage
-            className="size-8"
-            src='https://img.freepik.com/free-vector/smiling-boy-yellow-hoodie_1308-175701.jpg?semt=ais_hybrid&w=740' />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </div>
-    </nav>
+    <></>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,88 @@
-const Navbar  = () => {
+"use client"
+
+import * as React from "react"
+import { LogOut, Moon, SettingsIcon, Sun, User } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+
+
+
+const Navbar = () => {
+  const {theme, setTheme} = useTheme()
   return (
-    <div>Navbar </div>
+    <nav>
+      {/* Left */}
+      collapseButton
+      {/* Right */}
+      <div className="flex items-center gap-4">
+        <Link href='/dashboard'>Dashboard</Link>
+        {/* theme menu */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* user menu */}
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage
+                className="size-8"
+                src='https://img.freepik.com/free-vector/smiling-boy-yellow-hoodie_1308-175701.jpg?semt=ais_hybrid&w=740' />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>
+              My Account
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <SettingsIcon className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+
+
+      </div>
+    </nav>
   );
 };
 
-export default Navbar ;
+export default Navbar;
