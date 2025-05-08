31c7901cac7401cea704a42a951d218603a8f460
KhanRabiul <krabiulislam2@gmail.com>
2025-05-08 16:17:36 +0600

    ([username] page.tsx) hover card

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -1,4 +1,7 @@
+import CardList from "@/components/CardList";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 
 const SingleUserPage = () => {
   return (
@@ -18,6 +21,69 @@ const SingleUserPage = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      {/* Container */}
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        {/* Left */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* user badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Badges</h1>
+            <div className="flex gap-4 mt-4">
+              <HoverCard>
+                <HoverCardTrigger >
+                  <Shield size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
+                </HoverCardContent>
+              </HoverCard>
+              <HoverCard>
+                <HoverCardTrigger >
+                  <Citrus size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
+                </HoverCardContent>
+              </HoverCard>
+              <HoverCard>
+                <HoverCardTrigger >
+                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
+                </HoverCardContent>
+              </HoverCard>
+              <HoverCard>
+                <HoverCardTrigger >
+                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+          </div>
+          {/* Information container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+          {/* Card list container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+
+        </div>
+        {/* Right */}
+        <div className="w-full xl:w-2/3 space-y-6">
+          {/* user card container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
+          {/* chart container  */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+
+        </div>
+      </div>
     </div>
   );
 };
