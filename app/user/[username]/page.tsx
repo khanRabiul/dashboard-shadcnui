6d1e4c385ee3e7fd59ff522f9765d31cb7fb0910
KhanRabiul <krabiulislam2@gmail.com>
2025-05-09 23:07:58 +0600

    ([username]) page progress and info added

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -1,6 +1,8 @@
 import CardList from "@/components/CardList";
+import { Badge } from "@/components/ui/badge";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
 import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 
 const SingleUserPage = () => {
@@ -31,7 +33,7 @@ const SingleUserPage = () => {
             <div className="flex gap-4 mt-4">
               <HoverCard>
                 <HoverCardTrigger >
-                  <Shield size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                  <Shield size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2" />
                 </HoverCardTrigger>
                 <HoverCardContent>
                   <h1 className="font-bold mb-2">Verified User</h1>
@@ -40,7 +42,7 @@ const SingleUserPage = () => {
               </HoverCard>
               <HoverCard>
                 <HoverCardTrigger >
-                  <Citrus size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                  <Citrus size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2" />
                 </HoverCardTrigger>
                 <HoverCardContent>
                   <h1 className="font-bold mb-2">Verified User</h1>
@@ -49,7 +51,7 @@ const SingleUserPage = () => {
               </HoverCard>
               <HoverCard>
                 <HoverCardTrigger >
-                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2" />
                 </HoverCardTrigger>
                 <HoverCardContent>
                   <h1 className="font-bold mb-2">Verified User</h1>
@@ -58,7 +60,7 @@ const SingleUserPage = () => {
               </HoverCard>
               <HoverCard>
                 <HoverCardTrigger >
-                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2"/>
+                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-blue-500/50 p-2" />
                 </HoverCardTrigger>
                 <HoverCardContent>
                   <h1 className="font-bold mb-2">Verified User</h1>
@@ -68,7 +70,36 @@ const SingleUserPage = () => {
             </div>
           </div>
           {/* Information container */}
-          <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Information</h1>
+            <div className="space-y-4 mt-4">
+              <div className="flex flex-col gap-2 mb-8">
+                <p className="text-sm text-muted-foreground">Profile completion</p>
+                <Progress value={60} />
+              </div>
+              <div className="flex items-center">
+                <span className="font-bold">Username:</span>
+                <span>john.doe:</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-bold">Email:</span>
+                <span>john.doe@gmail.com</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-bold">Phone:</span>
+                <span>+1 234 5678</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-bold">Location:</span>
+                <span>New Yourk, NY</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-bold">Role:</span>
+                <Badge>Admin</Badge>
+              </div>
+            </div>
+            <p className="text-sm text-muted-foreground mt-4">Joined on 2025.01.01</p>
+          </div>
           {/* Card list container */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             <CardList title="Recent Transactions" />
