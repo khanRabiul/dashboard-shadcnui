54965cec21858e77d083bb8ded87c1176e4e96ad
KhanRabiul <krabiulislam2@gmail.com>
2025-05-09 23:35:24 +0600

    ([username]) page sheet or side bar moved to another component  and info added

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -6,13 +6,11 @@ import { Progress } from "@/components/ui/progress";
 import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 import {
   Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button";
+import EditUser from "@/components/EditUser";
+
 
 
 const SingleUserPage = () => {
@@ -87,15 +85,7 @@ const SingleUserPage = () => {
                 <SheetTrigger asChild>
                   <Button>Edit User</Button>
                 </SheetTrigger>
-                <SheetContent>
-                  <SheetHeader>
-                    <SheetTitle>Are you absolutely sure?</SheetTitle>
-                    <SheetDescription>
-                      This action cannot be undone. This will permanently delete your account
-                      and remove your data from our servers.
-                    </SheetDescription>
-                  </SheetHeader>
-                </SheetContent>
+                  <EditUser />
               </Sheet>
             </div>
             <div className="space-y-4 mt-4">
